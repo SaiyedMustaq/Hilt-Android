b498e7490838de0b739796978e49a74111be7d5e
MustaqSaiyed <mustaqsaiyed786@gmail.com>
2020-07-20 22:44:34 +0530

    -- row item ui changes

--- app/src/main/java/com/mustaq/hilt_demo/adapter/FactsViewHolder.kt
+++ app/src/main/java/com/mustaq/hilt_demo/adapter/FactsViewHolder.kt
@@ -10,27 +10,9 @@ import kotlinx.android.synthetic.main.row_item_facts.view.*
 
 class FactsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(list: PhotosModel.PhotosModelItem) {
-        itemView.tvId.text = list.id.toString()
-        itemView.tvAlbumId.text = list.albumId.toString()
-        itemView.tvTitle.text = list.title
-
-        Glide
-            .with(itemView.context)
-            .load(list.thumbnailUrl)
-            .centerCrop()
-            .placeholder(R.drawable.ic_plaseholder)
-            .error(R.drawable.ic_error_image)
-            .into(itemView.imagethumbnail)
-
-
-
-        Glide
-            .with(itemView.context)
-            .load(list.url)
-            .centerCrop()
-            .placeholder(R.drawable.ic_plaseholder)
-            .error(R.drawable.ic_error_image)
-            .into(itemView.imageUrl)
+        itemView.tvId.text = "Id:- " + list.id.toString()
+        itemView.tvAlbumId.text = "Album Id:- " + list.albumId.toString()
+        itemView.tvTitle.text = "Album Title:- "+list.title
     }
 
 }
